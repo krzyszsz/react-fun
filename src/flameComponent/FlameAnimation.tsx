9422c2f6a0bd82b91ec54dbe3e02d6c73624ed78
Krzysztof Janiszewski <krzysztofjaniszewski@users.noreply.github.com>
2019-05-09 00:03:16 +0100

    Continuation. Work in progress.

--- src/flameComponent/FlameAnimation.tsx
+++ src/flameComponent/FlameAnimation.tsx
@@ -4,150 +4,162 @@ export interface Props {
     fireWidth: number;
 }
 
-function normalizeColor(color : number) : number
-{
-    const max : number = 256;
-    return ~~(color*max);
-}
+class FlameAnimation extends React.Component<Props, object> {
+    private pixelSize : number = 3;
+    private sizeX : number = 150;
+    private sizeY : number = 50;
+    private imageArray : Array<Array<[number, number, number]>> = new Array<Array<[number, number, number]>>(this.sizeY);
+    
+    private canvas1 : HTMLCanvasElement | null = null;
+    private context1 : CanvasRenderingContext2D | null = null;
+    private canvas2 : HTMLCanvasElement | null = null;
+    private context2 : CanvasRenderingContext2D | null = null;
+    private firstFrameToRender : boolean = true;
+    private randomPrevious : number = 0;
+    private _timerID : number | null = null;
 
-function denormalizeColor(color : number) : number
-{
-    const max : number = 256;
-    return color / max;
-}
+    componentWillUnmount() {
+        if (this._timerID != null) {
+            clearInterval(this._timerID);
+        }
+      }
 
-const pixelSize : number = 2;
-const sizeX : number = 150;
-const sizeY : number = 50;
-const imageArray : Array<Array<[number, number, number]>> = new Array<Array<[number, number, number]>>(sizeY);
+    constructor(props: Props) {
+        super(props);
+        console.log("FlameAnimation initialized.");
+        
+        this.init = this.init.bind(this);
+        this.draw = this.draw.bind(this);
+        this.generateImageArray = this.generateImageArray.bind(this);
+        this.componentWillUnmount = this.componentWillUnmount.bind(this);
 
-let canvas1 : HTMLCanvasElement | null;
-let context1 : CanvasRenderingContext2D | null;
-let canvas2 : HTMLCanvasElement;
-let context2 : CanvasRenderingContext2D | null;
-let firstFrameToRender : boolean = true;
-let randomPrevious : number = 0;
-let _fireWidth : number;
+        window.requestAnimationFrame(this.draw);
+    }
 
-function init() : void {
-    if (canvas2 != null) {
+    render() {
+        return (
+            <div>
+                <canvas id="flameCanvas" width={this.sizeX*this.pixelSize} height={this.sizeY*this.pixelSize}>
+                </canvas>
+            </div>
+        );
+    }
+
+    normalizeColor(color : number) : number
+    {
+        const max : number = 256;
+        return ~~(color*max);
+    }
+    
+    denormalizeColor(color : number) : number
+    {
+        const max : number = 256;
+        return color / max;
+    }
+
+    init() : void {
+        if (this.canvas2 != null) {
+            return;
+        }
+    
+        // buffer canvas
+        this.canvas2 = document.createElement('canvas');
+        this.canvas2.width = this.sizeX * this.pixelSize;
+        this.canvas2.height = this.sizeY * this.pixelSize;
+        this.context2 = this.canvas2.getContext('2d');
+    
+        // real canvas
+        this.canvas1 = document.getElementById("flameCanvas") as HTMLCanvasElement;
+        if (this.canvas1 != null)
+        {
+            this.context1 = this.canvas1.getContext('2d');
+        }
+    
         return;
     }
-
-    // buffer canvas
-    canvas2 = document.createElement('canvas');
-    canvas2.width = sizeX * pixelSize;
-    canvas2.height = sizeY * pixelSize;
-    context2 = canvas2.getContext('2d');
-
-    // real canvas
-    canvas1 = document.getElementById("flameCanvas") as HTMLCanvasElement;
-    if (canvas1 != null)
+    
+    
+    generateImageArray() : void
     {
-        context1 = canvas1.getContext('2d');
+        if (this.firstFrameToRender) {
+            for (let x=0; x<this.sizeX; x++) {
+                this.imageArray[x] = new Array<[number, number, number]>(this.sizeY);
+                for (let y=0; y<this.sizeY; y++) {
+                    this.imageArray[x][y] = [0, 0, 0];
+                }
+            }
+    
+            this.firstFrameToRender = false;
+        }
+    
+        this.randomPrevious = this.randomPrevious * 0.8 - (Math.random() - 0.5) * 0.2;    // simulates wind and random air movements
+        if (this.randomPrevious < -1)
+        {
+            this.randomPrevious = -1;
+        }
+    
+        if (this.randomPrevious > 1)
+        {
+            this.randomPrevious = 1;
+        }
+    
+        for (let x=0; x<this.sizeX; x++) {
+            for (let y=0; y<this.sizeY-1; y++) {
+                //const r = normalizeColor(((x + y + t/10 ) % (sizeX+sizeY) ) / (sizeX+sizeY)) ;
+    
+                let localRandom1 = Math.random() * 0.3 + 0.9;
+                let localRandom2 = (Math.random() - 0.5)/2;
+                let localRandom3 = (Math.random() - 0.5)/2;
+                let localRandom4 = (Math.random() - 0.5) /2 + 1;
+                let localRandom5 = (Math.random() - 0.2) * 0.4 ;
+    
+                const newColor = this.denormalizeColor( 
+                    Math.max(0,
+    
+                    Math.sqrt(y / this.sizeY) *
+                    (
+                        (this.imageArray[x][y][0] * 0.08) + 
+                        (this.imageArray[x][y+1][0] * 0.9 * localRandom1) +
+                        (x > 1 ? this.imageArray[x-1][y][0] * (this.randomPrevious + localRandom2) * 0.5 : 0) +
+                        (x < this.sizeX-1 ? this.imageArray[x+1][y][0] * (-this.randomPrevious + localRandom3) * 0.5 : 0) +
+                        (y < this.sizeY-2 ? this.imageArray[x][y+2][0] * (localRandom5) * 0.5 : 0)
+                    ) * localRandom4
+                    ));
+    
+                const r = this.normalizeColor(newColor) ;
+                const g = 0;
+                const b = 0;
+                this.imageArray[x][y] = [r, g, b];
+            }
+    
+            // Initialization vector - line on the bottom of the fire:
+            this.imageArray[x][this.sizeY-1] = [this.normalizeColor(  Math.abs(Math.sin(x * (2 * Math.PI) / (this.props.fireWidth*2) )) ) ,
+                 0,
+                 0];
+        }
     }
 
-    return;
-}
-
-
-function generateImageArray() : void
-{
-    if (firstFrameToRender) {
-        for (let x=0; x<sizeX; x++) {
-            imageArray[x] = new Array<[number, number, number]>(sizeY);
-            for (let y=0; y<sizeY; y++) {
-                imageArray[x][y] = [0, 0, 0];
+    draw() : void {
+        this.init();
+    
+        if (this.context1 == null || this.context2 == null || this.canvas2 == null) {
+            console.log("Context == null. Exiting.");
+            return;
+        }
+    
+        this.generateImageArray();
+        for (let i=0; i<this.imageArray.length; i++) {
+            for (let j=0; j<this.imageArray[i].length; j++) {
+                this.context2.fillStyle = `rgb(${this.imageArray[i][j][0]},${this.imageArray[i][j][1]}, ${this.imageArray[i][j][2]})`;
+                this.context2.fillRect(i*this.pixelSize, j*this.pixelSize, this.pixelSize, this.pixelSize);
             }
         }
-
-        firstFrameToRender = false;
-    }
-
-    randomPrevious = randomPrevious * 0.8 - (Math.random() - 0.5) * 0.2;    // simulates wind and random air movements
-    if (randomPrevious < -1)
-    {
-        randomPrevious = -1;
-    }
-
-    if (randomPrevious > 1)
-    {
-        randomPrevious = 1;
-    }
-
-    for (let x=0; x<sizeX; x++) {
-        for (let y=0; y<sizeY-1; y++) {
-            //const r = normalizeColor(((x + y + t/10 ) % (sizeX+sizeY) ) / (sizeX+sizeY)) ;
-
-            let localRandom1 = Math.random() * 0.3 + 0.9;
-            let localRandom2 = (Math.random() - 0.5)/2;
-            let localRandom3 = (Math.random() - 0.5)/2;
-            let localRandom4 = (Math.random() - 0.5) /2 + 1;
-            let localRandom5 = (Math.random() - 0.2) * 0.4 ;
-
-            const newColor = denormalizeColor( 
-                Math.max(0,
-
-                Math.sqrt(y / sizeY) *
-                (
-                    (imageArray[x][y][0] * 0.08) + 
-                    (imageArray[x][y+1][0] * 0.9 * localRandom1) +
-                    (x > 1 ? imageArray[x-1][y][0] * (randomPrevious + localRandom2) * 0.5 : 0) +
-                    (x < sizeX-1 ? imageArray[x+1][y][0] * (-randomPrevious + localRandom3) * 0.5 : 0) +
-                    (y < sizeY-2 ? imageArray[x][y+2][0] * (localRandom5) * 0.5 : 0)
-                ) * localRandom4
-                ));
-
-            const r = normalizeColor(newColor) ;
-            const g = 0;
-            const b = 0;
-            imageArray[x][y] = [r, g, b];
-        }
-
-        // Initialization vector - line on the bottom of the fire:
-        imageArray[x][sizeY-1] = [normalizeColor(  Math.abs(Math.sin(x * (2 * Math.PI) / (_fireWidth*2) )) ) ,
-             0,
-             0];
-    }
-}
-
-
-
-function draw() : void 
-{
-    init();
-
-    if (context1 == null || context2 == null) {
-        console.log("Context == null. Exiting.");
-        return;
-    }
-
-    generateImageArray();
-    for (let i=0; i<imageArray.length; i++) {
-        for (let j=0; j<imageArray[i].length; j++) {
-            context2.fillStyle = `rgb(${imageArray[i][j][0]},${imageArray[i][j][1]}, ${imageArray[i][j][2]})`;
-            context2.fillRect(i*pixelSize, j*pixelSize, pixelSize, pixelSize);
+    
+        this.context1.drawImage(this.canvas2, 0, 0);
+        if (this._timerID == null) {
+            this._timerID = window.setInterval(()=> window.requestAnimationFrame(this.draw), 20);
         }
     }
-
-    context1.drawImage(canvas2, 0, 0);
-  
-    window.setTimeout(()=> window.requestAnimationFrame(draw), 10);
-}
-
-function FlameAnimation({ fireWidth = 20 }: Props) {
-    console.log("FlameAnimation initialized.");
-    _fireWidth = fireWidth;
-    window.requestAnimationFrame(draw);
-
-    return (
-        <div>
-            <canvas id="flameCanvas" width={sizeX*pixelSize} height={sizeY*pixelSize}>
-
-            </canvas>
-        </div>
-    );
 }
 
 export default FlameAnimation;
-
