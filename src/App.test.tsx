f75473a34563d588913000372fd9157597e85575
Krzysztof Janiszewski <krzysztofdotandrzejdotjaniszewskiat g m a i ldotc o m>
2019-06-10 00:44:17 +0100

    Fixing unit test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { IStoreState } from './types';
+import { fireReducer } from './reducers';
 
 it('renders without crashing', () => {
+  const store = createStore(fireReducer, {
+    fireWidth: 50
+  } as IStoreState);
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
